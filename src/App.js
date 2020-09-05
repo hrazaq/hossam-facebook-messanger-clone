63b39c91b02057ad272f773fd3912bc0089618a4
razaq-hossam <razaqhossam.contact@gmail.com>
2020-09-05 18:49:59 +0100

    Adding some CSS Styling Aa=nd ready to deployed on firebase

--- src/App.js
+++ src/App.js
@@ -1,47 +1,66 @@
 import React, { useState, useEffect } from 'react';
-import {Button, TextField, FormControl} from '@material-ui/core';
+import {Input, FormControl, IconButton} from '@material-ui/core';
 import './App.css';
 import Message from './Message';
+import db from './firebase';
+import firebase from "firebase";
+import FlipMove from 'react-flip-move';
+import SendIcon from '@material-ui/icons/Send';
 
 function App() {
   const [input, setInput] = useState();
-  const [messages, setMessages] = useState([
-    {username: 'Hiba', text: 'Laaaa abb gaema tsuwert mazal'},
-    {username: 'Hiba', text: 'Kanbghiiiik ♥️'},
-    {username: 'Hossam', text: 'Kanbghiiiik a hibaa bzzff 🔥♥️'},
-  ]);
+  const [messages, setMessages] = useState([]);
   const [username, setUsername] = useState('');
 
+  useEffect(() => {
+      // get all from firebase
+      //looping on documents;[doc.data] is an object of {username , message}
+      db.collection('messages')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot(snapshot =>{
+        setMessages(snapshot.docs.map(doc => ({id: doc.id ,message: doc.data()})))
+      });
+  }, [])
 
   useEffect(() => {
-      // setUsername(prompt('Please enter your username 👨‍💻'))
-      setUsername('Hossam')
+      setUsername(prompt('Please enter your username 👨‍💻'))
   }, [])
  
   const sendMessage = (event) => {
     event.preventDefault();
-    setMessages([...messages, {username: username, text: input}]);
+    // send to firebase
+    db.collection('messages').add({
+      message: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
     setInput('');
   }
 
   return (
     <div className="App">
-     <h1>Hello Hossam 🚀</h1>
+     <img src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100" alt="messange_logo"></img>
+     <h1>Hello Clever Hossam 
+       <span role="img" aria-label="rocket">🚀 !</span>
+     </h1>
      <h3>Welcome {username} !</h3>
-     <form>
-        <FormControl>
-          <TextField id="standard-basic" label='Your message 👌' variant="outlined" value={input} onChange={event => setInput(event.target.value)}/>&nbsp;&nbsp;
-          <Button type="submit" onClick={sendMessage} disabled={!input} variant="contained" color="primary">
-              Send Message
-          </Button>
+
+     <form className="app__form">
+        <FormControl className="app__formControl">
+          <Input className="app__input"  placeholder='Enter e message...' value={input} onChange={event => setInput(event.target.value)}/>&nbsp;&nbsp;
+          <IconButton className="app__iconButton" type="submit" onClick={sendMessage} disabled={!input} variant="contained" color="primary">
+            <SendIcon />
+          </IconButton>
         </FormControl>  
      </form>
-        
-      {
-        messages.map(message => (
-            <Message username={username} message={message}/>
-        ))
-      }
+     <FlipMove>
+        {
+          messages.map(({id, message}) => (
+              <Message key={id} username={username} message={message}/>
+          ))
+        }
+     </FlipMove>
+      
     </div>
   );
 }
